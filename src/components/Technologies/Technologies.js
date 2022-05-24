5dac54f0892da3a54e6697bc43c290c0dfc6ecfd
zhangandrew37 <zhangandrew37@gmail.com>
2022-05-24 18:17:50 -0400

    update: personal info

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -19,7 +19,7 @@ const Technologies = () => (
     <SectionDivider />
     <br />
     <SectionTitle>Technologies</SectionTitle>
-    <SectionText>I'm skilled at running it down top, mid, and bot!</SectionText>
+    <SectionText>I'm proficient in many languages!</SectionText>
     <List>
       <ListItem>
         <DiFirebase size="3rem" />
